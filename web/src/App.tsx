876d9b38d13c5d0c221821586617051b7b89d3ed
Willy Hsu <N01678692@humber.ca>
2025-11-11 16:24:03 -0500

    changed name

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,7 +9,7 @@ import DropInProgram from "./pages/DropInProgram";
 import Login from "./pages/(auth)/Login";
 import Signup from "./pages/(auth)/SignUp";
 import { ErrorBoundary } from "react-error-boundary";
-import { ErrorScreen} from "./components/errors/ErrorFallBack"
+import { ErrorScreen} from "./components/errors/ErrorScreen"
 import { AuthProvider } from "./context/AuthContext";
 
 const REACT_APP_SERVER_API = process.env.REACT_APP_SERVER_API || "localhost:3000";
